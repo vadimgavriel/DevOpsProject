afccf4cf77e1a8e065cfd80471b6638d692bd2a5
vadimgavriel <vadim.gavriel.devops@gmail.com>
2019-08-23 15:24:24 +0300

    Z

---
+++ DevOpsProjectWebApi/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevOpsProjectWebApi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DevOpsProjectWebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace DevOpsProjectWebApi.Controllers
+{
+    public class HomeController : ApiController
+    {            
+        [HttpGet]
+        [ActionName("Data")]
+        public IHttpActionResult Data()
+        {
+            return Ok("Hey");
+        }
+
+        [HttpGet]
+        [ActionName("Value")]
+        public IHttpActionResult Value()
+        {
+            return Ok("Hi");
+        }
+    }
+}
